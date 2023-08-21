25391d8f52a82bb8b87dace39a9f8b3f0e10342c
Ashish Khanal <AKhanal@marathonpetroleum.com>
2023-08-21 17:54:38 -0400

    Adds Solution folders

---
+++ src/Services/Catalog/Catalog.API/DTOs/ItemDTOs.cs
@@ -0,0 +1,7 @@
+﻿namespace Catalog.API.DTOs;
+
+public record CreateItemDto(string Name, string Description, decimal Price);
+
+public record ItemDto(Guid Id, string Name, string? Description, decimal Price, DateTimeOffset CreatedDate, DateTimeOffset? UpdatedDate);
+
+public record UpdateItemDto(string Name, string Description, decimal Price);
